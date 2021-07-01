883e6f62bc8b0ebb453a4039f7b456d8d73dd7aa
nedalalashqar <nedal.r.alashqar@gmail.com>
2021-07-01 19:50:19 +0300

    add

--- backend/controller/movies.js
+++ backend/controller/movies.js
@@ -1,23 +1,32 @@
 const axios=require('axios');
 require('dotenv').config();
+const Cache = require('../cache/cache');
+const movie = require('../models/movie.model');
 const WEATHER_BIT_API=process.env.WEATHER_BIT_API;
 
+
+const cacheObject = new Cache();
 app.get('/movies' , (req , res)=>{
     let city=req.query.city_name
-    let moviesRes =axios.get(`https://api.themoviedb.org/3/search/movie?api_key${MOVIES_API_KEY}&query=${city}`).then(response =>{
-      let movies=response.data.results;
-        let findMov = movies.map(mov =>{
-            return new Movies(mov);
-        });
-        res.json(findMov);
-    }).catch(errer=>res.send(error.message));
+    const reqKey = `movie-${city}`;
+
+    if (city){
+        if(cacheObject[reqKey]){
+            res.json(cacheObject[reqKey]);
+        }else{
+            let moviesRes =axios.get(`https://api.themoviedb.org/3/search/movie?api_key${MOVIES_API_KEY}&query=${city}`).then(response =>{
+                let movies=response.data.results;
+                  let findMov = movies.map(mov =>{
+                      return new Movies(mov);
+                  });
+                  res.json(findMov);
+              }).catch(errer=>res.send(error.message));
+        }
+    }else{
+        res.send('please provide the proper lat and lon ');
+    }
+   
 })
 
-class Movies{
-    constructor(moviesData){
-        this.title=moviesData.original_title;
-        this.votes=moviesData.vote_count
-        this.img='http://image.tmdb.org/t/p/w342'+moviesData.poster_path;
-        
-    }
-}
+
+module.exports = movies;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const weather =require('./data/weather.json');
+const movies =require('./data/movies.json')
 const app = express();
 const cors =require('cors');
 const axios=require('axios');
@@ -8,15 +9,13 @@ require('dotenv').config();
 const weather = require('./controller/weather');
 const PORT = process.env.PORT;
 const MOVIES_API_KEY=process.env.MOVIES_API_KEY;
-
-
 app.use(cors());
 
 app.get('/' ,(req ,res) => {res.send('hello world')});
 
 app.get('/weather', weather);
 
-app.get('/movie', movieController)
+app.get('/movies', movieController)
 
 app.listen(PORT, () =>{
      console.log(`starting at ${PORT}`)
